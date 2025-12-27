81b9a8f6b75d8347b8fa3aaab06e29179fe9ba9e
Charles Lutz <103493003+CharlesCarMichaelLutz@users.noreply.github.com>
2025-12-27 16:11:40 +0800

    added Npgsql nuget package and implemented DB connection logic

---
+++ api/Data/SqlConnectionFactory.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace api.Data
+{
+    public interface ISqlConnectionFactory
+    {
+        Task<IDbConnection> CreateConnectionAsync();
+    }
+    public class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly string _connectionString;
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+
+            try
+            {
+                await connection.OpenAsync();
+
+            }
+            catch
+            {
+                throw new NpgsqlException();
+            }
+            return connection;
+        }
+    }
+}
