d9495b42a3b0e1439a8cf9d332eb501294ffdc34
Charles Lutz <103493003+CharlesCarMichaelLutz@users.noreply.github.com>
2026-01-06 16:41:29 +0800

    added xUnit project

---
+++ api.tests/Services/MessageServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using api.Models.Message;
+using api.Repositories;
+using api.Services;
+using FakeItEasy;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.tests.Services
+{
+    public class MessageServiceTests
+    {
+        private readonly MessageService _messageService;
+        private readonly IMessageRepository _messageRepository;
+        public MessageServiceTests()
+        {
+            _messageRepository = A.Fake<IMessageRepository>();
+            _messageService = new MessageService(_messageRepository);
+        }
+
+        [Fact]
+        public void 
+
+    }
+}
