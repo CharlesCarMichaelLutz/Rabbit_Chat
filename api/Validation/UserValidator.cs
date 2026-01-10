6fd03394324ba06f86b8092042e191c02a9e035e
Charles Lutz <103493003+CharlesCarMichaelLutz@users.noreply.github.com>
2026-01-10 20:56:29 +0800

    made changes to User validation

--- api/Validation/UserValidator.cs
+++ api/Validation/UserValidator.cs
@@ -10,13 +10,13 @@ namespace api.Validation
         {
             RuleFor(x => x.UserName)
                 .NotEmpty()
-                .Length(12).WithMessage("Username must be 12 characters long")
+                .Length(12, 30).WithMessage("Username minimum of 12 characters and maximum of 30")
                 .Matches(new Regex(@"^(?=.*[!@#$%^&*()_+{}:""<>?~`\-=[$$\\;',./])(?=.*[A-Z])(?=.*\d).{12}$"))
                 .WithMessage("1 special character,1 uppercase letter,and 1 number.");
 
             RuleFor(x => x.Password)
                 .NotEmpty()
-                .Length(12).WithMessage("Password must be 12 characters long")
+                .Length(12, 30).WithMessage("Password minimum of 12 characters and maximum of 30")
                 .Matches(new Regex(@"^(?=.*[!@#$%^&*()_+{}:""<>?~`\-=[$$\\;',./])(?=.*[A-Z])(?=.*\d).{12}$"))
                 .WithMessage("1 special character,1 uppercase letter,and 1 number.");
         }
